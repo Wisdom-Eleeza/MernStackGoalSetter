0580163878f0dacd7ae096bd7a4881e9f31de8dc
wisdom.eleeza@amalitech.com <wisdom.eleeza@amalitech.com>
2023-05-25 07:44:58 +0000

    REST API for goals setter

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,4 +1,3 @@
-// const { Console } = require('console')
 const mongoose = require('mongoose')
  
 const connectDB = async () => {
--- Backend/models/goalModel.js
+++ Backend/models/goalModel.js
@@ -1,8 +1,14 @@
-// const { timeStamp } = require("console");
 const mongoose = require("mongoose");
 
+// with every goal we have to know which user created that goal
+//every goal is going to be associated with a specific user
 const goalSchema = mongoose.Schema(
   {
+    user: { 
+      type: mongoose.Schema.Types.ObjectId, 
+      required: true,
+      ref: 'User' 
+    },
     text: {
       type: String,
       required: [true, "Please add a text value "],
